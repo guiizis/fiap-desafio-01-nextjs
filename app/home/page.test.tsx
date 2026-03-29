5fa15658c41f845de9c546147b01d47c6c4cfed0
guiziis <guigamoda@hotmail.com>
2026-03-29 18:54:51 -0300

    chore(home): adiciona svgs para pagina

--- app/home/page.test.tsx
+++ app/home/page.test.tsx
@@ -7,5 +7,14 @@ describe("HomePage", () => {
     render(<HomePage />);
 
     expect(screen.getByRole("main")).toBeInTheDocument();
+    expect(
+      screen.getByRole("heading", {
+        name: /Experimente mais liberdade no controle da sua vida financeira/i,
+        level: 1,
+      })
+    ).toBeInTheDocument();
+    expect(
+      screen.getByRole("heading", { name: "Vantagens do nosso banco:", level: 2 })
+    ).toBeInTheDocument();
   });
 });
