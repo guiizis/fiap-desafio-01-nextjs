0b413335cfb2917aba7ce3c43fa136ddd9682fe8
guiziis <guigamoda@hotmail.com>
2026-03-29 18:58:50 -0300

    feat(home): adiciona componets hero na home

--- app/home/page.tsx
+++ app/home/page.tsx
@@ -1,11 +1,17 @@
 import { HomeHeader } from "./_components/home-header";
 import { HomeFooter } from "./_components/home-footer";
+import { BenefitsSection } from "./_sections/benefits-section";
+import { HeroSection } from "./_sections/hero-section";
 
 export default function HomePage() {
   return (
-    <main className="flex flex-1 flex-col">
+    <div className="flex flex-1 flex-col">
       <HomeHeader />
+      <main className="flex-1 bg-[linear-gradient(180deg,var(--color-primary)_0%,var(--color-background)_72%)]">
+        <HeroSection />
+        <BenefitsSection />
+      </main>
       <HomeFooter />
-    </main>
+    </div>
   );
 }
