ca575b9aad1d5758b86be32cd036ae8af1111ab4
guiziis <guigamoda@hotmail.com>
2026-03-29 10:51:30 -0300

    chore(commitlint): adiciona escopo de test

--- commitlint.config.js
+++ commitlint.config.js
@@ -1,7 +1,7 @@
 module.exports = {
   extends: ["@commitlint/config-conventional"],
   rules: {
-    "type-enum": [2, "always", ["chore", "feat", "fix", "refactor", "docs"]],
+    "type-enum": [2, "always", ["chore", "feat", "fix", "refactor", "docs", "test"]],
     "scope-empty": [2, "never"],
     "subject-empty": [2, "never"],
     "subject-case": [2, "never", ["sentence-case", "start-case", "pascal-case", "upper-case"]],
