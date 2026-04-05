bce67a6e20c7b90266b20b4b61f5bdf52cc5950f
guiziis <guigamoda@hotmail.com>
2026-04-05 14:22:03 -0300

    feat(404): adiciona tela de not found

--- app/home/page.tsx
+++ app/home/page.tsx
@@ -1,17 +1,11 @@
-import { HomeHeader } from "./_components/home-header";
-import { HomeFooter } from "./_components/home-footer";
 import { BenefitsSection } from "./_sections/benefits-section";
 import { HeroSection } from "./_sections/hero-section";
 
 export default function HomePage() {
   return (
-    <div className="flex flex-1 flex-col">
-      <HomeHeader />
-      <main className="flex-1 bg-[linear-gradient(180deg,var(--color-primary)_0%,var(--color-background)_72%)]">
-        <HeroSection />
-        <BenefitsSection />
-      </main>
-      <HomeFooter />
-    </div>
+    <main className="flex-1 bg-[linear-gradient(180deg,var(--color-primary)_0%,var(--color-background)_72%)]">
+      <HeroSection />
+      <BenefitsSection />
+    </main>
   );
 }
