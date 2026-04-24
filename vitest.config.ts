93dbb0bd481b8725ebc73c83feadec3ed34e1cef
Thaís Oliveira <jts_oliveira@outlook.com>
2026-04-24 09:37:28 -0300

    test: fix test config to accept alias

--- vitest.config.ts
+++ vitest.config.ts
@@ -1,25 +1,31 @@
-import { defineConfig } from "vitest/config";
+import { defineConfig } from 'vitest/config';
+import path from 'path';
 
 export default defineConfig({
+  resolve: {
+    alias: {
+      '@': path.resolve(__dirname, './'),
+    },
+  },
   test: {
-    environment: "jsdom",
+    environment: 'jsdom',
     globals: true,
-    setupFiles: "./vitest.setup.ts",
-    include: ["**/*.{test,spec}.{js,mjs,cjs,ts,mts,cts,jsx,tsx}"],
+    setupFiles: './vitest.setup.ts',
+    include: ['**/*.{test,spec}.{js,mjs,cjs,ts,mts,cts,jsx,tsx}'],
     coverage: {
-      provider: "v8",
-      reporter: ["text", "lcov"],
-      include: ["app/**", "components/ui/**/*.{ts,tsx}"],
+      provider: 'v8',
+      reporter: ['text', 'lcov'],
+      include: ['app/**', 'components/ui/**/*.{ts,tsx}'],
       all: true,
       exclude: [
-        "node_modules/**",
-        ".next/**",
-        "coverage/**",
-        "**/coverage/**",
-        "**/*.test.{ts,tsx}",
-        "**/*.stories.{ts,tsx}",
-        "**/index.ts",
-        "**/*.interfaces.ts"
+        'node_modules/**',
+        '.next/**',
+        'coverage/**',
+        '**/coverage/**',
+        '**/*.test.{ts,tsx}',
+        '**/*.stories.{ts,tsx}',
+        '**/index.ts',
+        '**/*.interfaces.ts',
       ],
       thresholds: {
         statements: 99,
